c0aa538f0f9307a7c9d6edec7c4e0f2d24963684
Caius Durling <dev@caius.name>
2014-04-30 22:50:04 +0100

    Use applescript to import URL

---
+++ bin/import_url_to_evernote.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+url="${1}"
+
+osascript <<APPLESCRIPT
+tell application "Evernote"
+  set NotebookName to "Reading List Archive"
+
+  set urlToStore to "${url}"
+
+  set query to "notebook:\"" & NotebookName & "\" sourceURL:\"" & urlToStore & "\""
+
+  set found_notes to find notes query
+
+  if (length of found_notes) = 0 then
+    create note from url urlToStore notebook NotebookName
+    return "CREATED " & urlToStore
+  end if
+end tell
+APPLESCRIPT
