79c9016207c9345dcb009522ba4aea649fc21753
Caius Durling <dev@caius.name>
2014-04-30 22:50:25 +0100

    Archive using the evernote API

---
+++ bin/archive_to_evernote.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+URLS = ARGF.read.split("\n")
+
+if URLS.empty?
+  fail "No URLs passed"
+end
+
+require "evernote_oauth"
+
+NOTEBOOK_NAME = "Archive".downcase
+
+def create_note_for(url)
+  note = Evernote::EDAM::Type::Note.new(
+    :title => url,
+    :content => %{<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note><![CDATA[#{url}]]></en-note>},
+    :notebookGuid => NOTEBOOK_GUID,
+    :attributes => Evernote::EDAM::Type::NoteAttributes.new(
+      :sourceApplication => "ReadingList",
+      :sourceURL => url,
+    ),
+  )
+  NOTE_STORE.createNote(TOKEN, note)
+end
+
+def notes_for(url)
+  query = Evernote::EDAM::NoteStore::NoteFilter.new(
+    :notebookGuid => NOTEBOOK_GUID,
+    :words => %{sourceUrl:"#{url}"},
+  )
+
+  result_spec = Evernote::EDAM::NoteStore::NotesMetadataResultSpec.new(
+    :includeAttributes => true,
+    :includeDeleted => true,
+    :includeNotebookGuid => true,
+  )
+
+  NOTE_STORE.findNotesMetadata(TOKEN, query, 0, 1, result_spec).totalNotes
+end
+
+# Creds
+TOKEN = File.read(File.join(ENV["HOME"], ".evernote-token")).strip
+
+# Set up the NoteStore client
+client = EvernoteOAuth::Client.new(
+token: TOKEN,
+)
+NOTE_STORE = client.note_store
+
+NOTEBOOK = NOTE_STORE.listNotebooks.find {|x| x.name.downcase == NOTEBOOK_NAME }
+
+unless NOTEBOOK
+  fail "Couldn't find notebook called #{NOTEBOOK_NAME.inspect}"
+end
+
+NOTEBOOK_GUID = NOTEBOOK.guid
+
+URLS.each do |url|
+  create_note_for(url) if notes_for(url).zero?
+end
